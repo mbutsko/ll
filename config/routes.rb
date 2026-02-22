775f60b9222de9aae425c471920c9ae07e08303c
Mike Butsko <mike.butsko@healiahealth.com>
2026-02-22 07:41:20 -0500

    Journals

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   resources :labels, except: [:show]
   resources :journal_entries, only: [:create, :destroy]
 
+  get   "measurements/new",      to: "measurements#new",         as: :new_measurement
+  get   "measurements/:id/edit", to: "measurements#edit",        as: :edit_measurement
+  patch "measurements/:id",      to: "measurements#update",      as: :update_measurement
+  post  "measurements",          to: "measurements#full_create", as: :create_measurement
+
   get  "measurements/:metric_slug", to: "measurements#index", as: :measurement
   post "measurements/:metric_slug", to: "measurements#create", as: :measurements
 
